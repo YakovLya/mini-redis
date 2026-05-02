238b78e364194fc861d06193115d38ae3a590dee
yakovlya <yakaulyahovich@gmail.com>
2026-05-02 08:58:53 +0300

    refactor: server own class

---
+++ src/server/server.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "commands/processor.hpp"
+#include "storage/storage.hpp"
+#include <chrono>
+#include <deque>
+
+struct ClientBuffer{
+    std::deque<char> buffer;
+    std::chrono::steady_clock::time_point last_activity;
+};
+
+class Server{
+private:
+    int server_fd = -1;
+    int epoll_fd = -1;
+    Storage storage_;
+    CommandProcessor processor_;
+    std::unordered_map<int, ClientBuffer> client_buffers_;
+
+    bool new_connection();
+    bool new_bytes(int client_fd);
+    void clean_idle_clients();
+    
+public:
+    explicit Server(int port, Storage db, CommandProcessor processor);
+    void run();
+    ~Server();
+};
